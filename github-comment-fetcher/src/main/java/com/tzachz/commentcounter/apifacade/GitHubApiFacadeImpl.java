52baf66b3b657d3c8b4eab111ccac2a3398b20c8
Tzach Zohar <tzach.zohar@kenshoo.com>
2013-08-23 22:34:16 +0300

    don't retain web resource - only client

--- github-comment-fetcher/src/main/java/com/tzachz/commentcounter/apifacade/GitHubApiFacadeImpl.java
+++ github-comment-fetcher/src/main/java/com/tzachz/commentcounter/apifacade/GitHubApiFacadeImpl.java
@@ -2,8 +2,6 @@ package com.tzachz.commentcounter.apifacade;
 
 import com.tzachz.commentcounter.apifacade.jsonobjects.GHComment;
 import com.tzachz.commentcounter.apifacade.jsonobjects.GHPullRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.Date;
@@ -16,8 +14,6 @@ import java.util.Set;
  * Time: 14:44
  */
 public class GitHubApiFacadeImpl extends GitHubResource implements GitHubApiFacade {
-
-    private final static Logger logger = LoggerFactory.getLogger(GitHubApiFacadeImpl.class);
 
     private final OrgResource orgResource;
     private final OrgRepositoriesResource orgRepositoriesResource;
@@ -47,7 +43,6 @@ public class GitHubApiFacadeImpl extends GitHubResource implements GitHubApiFaca
 
     @Override
     public GHPullRequest getPullRequest(String url) {
-        logger.info("fetching URL: " + url);
-        return getClient().resource(url).get(GHPullRequest.class);
+        return getResource(url, GHPullRequest.class);
     }
 }
