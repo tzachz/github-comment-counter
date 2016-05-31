c201fd17bfd5f55eb785524950d5496d2e1270d1
Toby Hsieh <thrasher13b@gmail.com>
2016-05-30 19:47:31 -0700

    Change PullRequestResource to return null on 404

--- github-comment-fetcher/src/test/java/com/tzachz/commentcounter/apifacade/PullRequestResourceTest.java
+++ github-comment-fetcher/src/test/java/com/tzachz/commentcounter/apifacade/PullRequestResourceTest.java
@@ -2,10 +2,14 @@ package com.tzachz.commentcounter.apifacade;
 
 import com.tzachz.commentcounter.apifacade.jsonobjects.GHPullRequest;
 import com.tzachz.commentcounter.apifacade.jsonobjects.GHUser;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class PullRequestResourceTest {
@@ -13,10 +17,22 @@ public class PullRequestResourceTest {
     @Rule
     public VMOptsCredentials credentials = new VMOptsCredentials();
 
+    private PullRequestResource resource;
+
+    @Before
+    public void setUp() {
+        resource = new PullRequestResource(credentials);
+    }
+
     @Test
     public void pullRequestFetched() throws Exception {
-        PullRequestResource resource = new PullRequestResource(credentials);
         GHPullRequest result = resource.getPullRequest("https://api.github.com/repos/kenshoo/facterj/pulls/1");
         assertThat(result.getUser(), equalTo(new GHUser("AvihayTsayeg", "")));
+    }
+
+    @Test
+    public void getPullRequestOn404() {
+        GHPullRequest result = resource.getPullRequest("https://api.github.com/repos/rails/rails/pulls/1");
+        assertThat(result, is(nullValue()));
     }
 }
