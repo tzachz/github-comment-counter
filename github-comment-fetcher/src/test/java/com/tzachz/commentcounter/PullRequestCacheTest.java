312781d88b3cb67b9aadcaee7adc3df0eb4f4be1
Toby Hsieh <thrasher13b@gmail.com>
2016-06-01 00:18:25 -0700

    Change PullRequestCache#get to return null on not found

--- github-comment-fetcher/src/test/java/com/tzachz/commentcounter/PullRequestCacheTest.java
+++ github-comment-fetcher/src/test/java/com/tzachz/commentcounter/PullRequestCacheTest.java
@@ -8,7 +8,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -43,10 +45,19 @@ public class PullRequestCacheTest {
     @Test
     public void successfulFetchOccursOnce() throws Exception {
         when(facade.getPullRequest("url")).thenReturn(new GHPullRequest(new GHUser("user1", "")));
-        cache.get("url");
-        cache.get("url");
-        verify(facade, times(1)).getPullRequest("url");
-        verifyNoMoreInteractions(facade);
+        verifyGetPullRequestCalledOnce();
+    }
+
+    @Test
+    public void notFoundReturnsNull() {
+        when(facade.getPullRequest("url")).thenReturn(null);
+        assertThat(cache.get("url"), is(nullValue()));
+    }
+
+    @Test
+    public void notFoundFetchesOnce() {
+        when(facade.getPullRequest("url")).thenReturn(null);
+        verifyGetPullRequestCalledOnce();
     }
 
     @Test
@@ -62,5 +73,12 @@ public class PullRequestCacheTest {
         when(facade.getPullRequest("url")).thenThrow(new RuntimeException("can't access url"));
         GHPullRequest result = cache.get("url");
         assertThat(result, equalTo(PullRequestCache.UNKNOWN));
+    }
+
+    private void verifyGetPullRequestCalledOnce() {
+        cache.get("url");
+        cache.get("url");
+        verify(facade, times(1)).getPullRequest("url");
+        verifyNoMoreInteractions(facade);
     }
 }
